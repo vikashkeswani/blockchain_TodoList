2c6df9a78f8d8a49f7a6953464cfe3a2aa6c1f9f
vikash keswani <vikashkeswani@gmail.com>
2020-04-21 09:57:23 -0700

    Todolist Project Setup

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -2,4 +2,12 @@ pragma solidity ^0.5.0 ;
 
 contract TodoList {
     uint public taskCount = 0;
+
+    struct Task {
+        uint id ;
+        string content ;
+        bool completed ;
+    }
+
+    mapping (uint => Task) public tasks ;
  } 
